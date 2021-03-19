0ffcdf9bfc5223aafc85d3ace9ee5e424fac582e
Lucky Prabath <lucky.prabath94@gmail.com>
2021-03-19 16:48:32 +0530

    Add exception handler

---
+++ src/main/java/lk/ijse/dep/web/institute/api/exception/AppWideExceptionHandler.java
@@ -0,0 +1,26 @@
+package lk.ijse.dep.web.institute.api.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author : Lucky Prabath <lucky.prabath94@gmail.com>
+ * @since : 2021-03-19
+ **/
+
+@RestControllerAdvice
+public class AppWideExceptionHandler {
+
+    Logger logger = LoggerFactory.getLogger(AppWideExceptionHandler.class);
+    
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Throwable.class)
+    public String globalExceptionHandler(Throwable t){
+        logger.error(null,t);
+        return "Something went wrong, contact lucky";
+    }
+}
